9a0b80954db4b345086fc2f5854534f0880fda32
pawoo0211 <pawoo0211@naver.com>
2024-04-06 20:20:12 +0900

    Test: 문자열을 정수로 바꾸기

---
+++ test/study/algorithm/programmers/level1/StringToNumberConverterTest.java
@@ -0,0 +1,31 @@
+package study.algorithm.programmers.level1;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("문자열 정수로 바꾸기")
+public class StringToNumberConverterTest {
+    private StringToNumberConverter converter = new StringToNumberConverter();
+
+    @Test
+    void convert() {
+        String givenNuber = "1234";
+        Assertions.assertThat(converter.convert(givenNuber)).isEqualTo(1234);
+    }
+
+    @Test
+    void convertNegative() {
+        String givenNumber = "-1234";
+        Assertions.assertThat(converter.convert(givenNumber))
+                .isEqualTo(-1234);
+    }
+
+    class StringToNumberConverter {
+
+        public Integer convert(String stringNumber) {
+            return Integer.valueOf(stringNumber);
+        }
+    }
+}
