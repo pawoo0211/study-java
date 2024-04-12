3868b3a0dc4b96e7eb0348c47b9b2c64eafe5dc4
samuel_eximbay <samuel@eximbay.com>
2024-04-12 12:53:41 +0900

    refactor: 약수의 합

---
+++ src/study/algorithm/programmers/level1/SumFactors.java
@@ -0,0 +1,40 @@
+package study.algorithm.programmers.level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SumFactors {
+    private final int number;
+    private final int lastDivision;
+
+    public SumFactors(int number) {
+        this.number = number;
+        lastDivision =  number / 2;
+    }
+
+    public int sum() {
+        int result = 0;
+        List<Integer> factors = findFactors();
+        for (Integer factor : factors) {
+            result = result + factor;
+        }
+        return result;
+    }
+
+    public List<Integer> findFactors() {
+        List<Integer> factors = initializeFactors();
+        for (int division = 2; division <= lastDivision; division++) {
+            if (number % division == 0) {
+                factors.add(division);
+            }
+        }
+        return factors;
+    }
+
+    private List<Integer> initializeFactors() {
+        List<Integer> factors = new ArrayList<>();
+        factors.add(1);
+        factors.add(number);
+        return factors;
+    }
+}
--- test/study/algorithm/programmers/level1/SumFactorsTest.java
+++ test/study/algorithm/programmers/level1/SumFactorsTest.java
@@ -6,17 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @DisplayName("약수의 합")
 public class SumFactorsTest {
-
-    @Test
-    void inputZero() {
-        SumFactors sumFactors = new SumFactors(0);
-        System.out.println(sumFactors.getLastDivision());
-    }
 
     @ParameterizedTest
     @ValueSource(ints = {1,2,5,10})
@@ -42,40 +35,5 @@ public class SumFactorsTest {
     void sum_input_5() {
         SumFactors sumFactors = new SumFactors(5);
         Assertions.assertThat(sumFactors.sum()).isEqualTo(6);
-    }
-
-    class SumFactors {
-        private final int number;
-        private final int lastDivision;
-
-        public SumFactors(int number) {
-            this.number = number;
-            lastDivision =  number / 2;
-        }
-
-        public List<Integer> findFactors() {
-            List<Integer> factors = new ArrayList<>();
-            factors.add(1);
-            factors.add(number);
-            for (int division = 2; division <= lastDivision; division++) {
-                if (number % division == 0) {
-                    factors.add(division);
-                }
-            }
-            return factors;
-        }
-
-        public int sum() {
-            int result = 0;
-            List<Integer> factors = findFactors();
-            for (Integer factor : factors) {
-                result = result + factor;
-            }
-            return result;
-        }
-
-        public int getLastDivision() {
-            return lastDivision;
-        }
     }
 }
