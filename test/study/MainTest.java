b16b7015ff1c7902f76ac651ac8828e8770d8fc8
pawoo0211 <pawoo0211@naver.com>
2024-07-28 14:20:54 +0900

    test: 임시 테스트

---
+++ test/study/MainTest.java
@@ -0,0 +1,12 @@
+package study;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("메인 테스트")
+public class MainTest {
+
+    @Test
+    void 임시_테스트() throws Exception {
+    }
+}
