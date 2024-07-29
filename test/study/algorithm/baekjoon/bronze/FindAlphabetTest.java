212b37c1a186593259aab99b3026edea036a17e2
pawoo0211 <pawoo0211@naver.com>
2024-07-29 20:34:59 +0900

    feat: 알파벳 찾기

--- test/study/algorithm/baekjoon/bronze/FindAlphabetTest.java
+++ test/study/algorithm/baekjoon/bronze/FindAlphabetTest.java
@@ -1,28 +1,43 @@
-package study;
+package study.algorithm.baekjoon.bronze;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.util.HashMap;
 import java.util.Map;
 
-@DisplayName("메인 테스트")
-public class MainTest {
+@DisplayName("알파벳 찾기 테스트")
+public class FindAlphabetTest {
+    private FindAlphabet findAlphabet;
+
+    @BeforeEach
+    void setUp() {
+        findAlphabet = new FindAlphabet();
+    }
 
     @Test
-    void 임시_테스트() throws Exception {
+    void case1() {
         String input = "baekjoon";
         String answer = "1 0 -1 -1 2 -1 -1 -1 -1 4 3 -1 -1 7 5 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1";
+
+        String result = findAlphabet.find(input);
+
+        Assertions.assertThat(result).isEqualTo(answer);
+    }
+}
+
+class FindAlphabet {
+
+    public String find(String word) {
         Map<String, Integer> alphabetMap = new HashMap<>();
 
         for (char alphabet = 'a'; alphabet <= 'z'; alphabet++) {
             alphabetMap.put(String.valueOf(alphabet), -1);
         }
 
-        String[] splitInput = input.split("");
+        String[] splitInput = word.split("");
         for (int index = 0; index < splitInput.length; index++) {
             String alphabet = splitInput[index];
             if (alphabetMap.get(alphabet).equals(-1)) {
@@ -34,9 +49,7 @@ public class MainTest {
         for (Integer alphabetIndex : alphabetMap.values()) {
             result = result + String.valueOf(alphabetIndex) + " ";
         }
-        result = result.substring(0, result.length() -1);
 
-        System.out.println("result : " + result);
-        Assertions.assertThat(result).isEqualTo(answer);
+        return result.substring(0, result.length() -1);
     }
 }
